c152c371876c452064fecf7575afc509b36da17f
Aldo Bertero <thescientist14@gmail.com>
2016-09-08 11:43:00 -0300

    Complementa pruebas
    Se complementa clase de prueba para comparar manos.

--- ejercicio-2/poker-webapp/src/test/com/comparaonline/beans/HandTest.java
+++ ejercicio-2/poker-webapp/src/test/com/comparaonline/beans/HandTest.java
@@ -159,4 +159,98 @@ public class HandTest {
         Assert.assertEquals(hand1.compareTo(hand2), -1);
     }
 
+    @Test
+    public void compareTwoPairs() {
+        Hand hand1 = HandRankingTest.createTestHand(
+                new Card(Suit.CLUBS, CardValue.EIGHT),
+                new Card(Suit.HEARTS, CardValue.SIX),
+                new Card(Suit.SPADES, CardValue.SIX),
+                new Card(Suit.DIAMONDS, CardValue.FIVE),
+                new Card(Suit.CLUBS, CardValue.FIVE)
+        );
+        Hand hand2 = HandRankingTest.createTestHand(
+                new Card(Suit.DIAMONDS, CardValue.EIGHT),
+                new Card(Suit.CLUBS, CardValue.SEVEN),
+                new Card(Suit.HEARTS, CardValue.SEVEN),
+                new Card(Suit.SPADES, CardValue.NINE),
+                new Card(Suit.DIAMONDS, CardValue.NINE)
+        );
+        Assert.assertEquals(hand1.compareTo(hand2), -1);
+    }
+
+    @Test
+    public void compareTwoPairsEqualsOnePair() {
+        Hand hand1 = HandRankingTest.createTestHand(
+                new Card(Suit.CLUBS, CardValue.EIGHT),
+                new Card(Suit.HEARTS, CardValue.SIX),
+                new Card(Suit.SPADES, CardValue.SIX),
+                new Card(Suit.HEARTS, CardValue.NINE),
+                new Card(Suit.CLUBS, CardValue.NINE)
+        );
+        Hand hand2 = HandRankingTest.createTestHand(
+                new Card(Suit.DIAMONDS, CardValue.EIGHT),
+                new Card(Suit.CLUBS, CardValue.FIVE),
+                new Card(Suit.HEARTS, CardValue.FIVE),
+                new Card(Suit.SPADES, CardValue.NINE),
+                new Card(Suit.DIAMONDS, CardValue.NINE)
+        );
+        Assert.assertEquals(hand1.compareTo(hand2), 1);
+    }
+
+    @Test
+    public void testTwoPairsEquals() {
+        Hand hand1 = HandRankingTest.createTestHand(
+                new Card(Suit.DIAMONDS, CardValue.EIGHT),
+                new Card(Suit.DIAMONDS, CardValue.FIVE),
+                new Card(Suit.SPADES, CardValue.FIVE),
+                new Card(Suit.HEARTS, CardValue.NINE),
+                new Card(Suit.CLUBS, CardValue.NINE)
+        );
+        Hand hand2 = HandRankingTest.createTestHand(
+                new Card(Suit.CLUBS, CardValue.SEVEN),
+                new Card(Suit.CLUBS, CardValue.FIVE),
+                new Card(Suit.HEARTS, CardValue.FIVE),
+                new Card(Suit.SPADES, CardValue.NINE),
+                new Card(Suit.DIAMONDS, CardValue.NINE)
+        );
+        Assert.assertEquals(hand1.compareTo(hand2), 1);
+    }
+
+    @Test
+    public void testOnePair() {
+        Hand hand1 = HandRankingTest.createTestHand(
+                new Card(Suit.DIAMONDS, CardValue.EIGHT),
+                new Card(Suit.DIAMONDS, CardValue.FIVE),
+                new Card(Suit.SPADES, CardValue.FIVE),
+                new Card(Suit.HEARTS, CardValue.TEN),
+                new Card(Suit.CLUBS, CardValue.NINE)
+        );
+        Hand hand2 = HandRankingTest.createTestHand(
+                new Card(Suit.CLUBS, CardValue.SEVEN),
+                new Card(Suit.CLUBS, CardValue.FIVE),
+                new Card(Suit.HEARTS, CardValue.JACK),
+                new Card(Suit.SPADES, CardValue.NINE),
+                new Card(Suit.DIAMONDS, CardValue.NINE)
+        );
+        Assert.assertEquals(hand1.compareTo(hand2), -1);
+    }
+
+    @Test
+    public void testHighCard() {
+        Hand hand1 = HandRankingTest.createTestHand(
+                new Card(Suit.DIAMONDS, CardValue.EIGHT),
+                new Card(Suit.DIAMONDS, CardValue.FIVE),
+                new Card(Suit.SPADES, CardValue.FOUR),
+                new Card(Suit.HEARTS, CardValue.TEN),
+                new Card(Suit.CLUBS, CardValue.NINE)
+        );
+        Hand hand2 = HandRankingTest.createTestHand(
+                new Card(Suit.CLUBS, CardValue.SEVEN),
+                new Card(Suit.CLUBS, CardValue.FIVE),
+                new Card(Suit.HEARTS, CardValue.JACK),
+                new Card(Suit.SPADES, CardValue.NINE),
+                new Card(Suit.DIAMONDS, CardValue.QUEEN)
+        );
+        Assert.assertEquals(hand1.compareTo(hand2), -1);
+    }
 }
